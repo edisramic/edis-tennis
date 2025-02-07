f915aed13706252d0222d9aa1f613278ac77a9f2
EDIS RAMIC <edisramic@hotmail.com>
2025-02-07 16:49:27 +1100

    Remove compiled files

--- src/utils/generatePoint.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generatePoint = generatePoint;
-function generatePoint() {
-    return Math.random() < 0.5 ? "Player A" : "Player B";
-}
